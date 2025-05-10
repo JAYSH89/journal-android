8d326b52d2bdb36a62c0c249424f50a9e9c1d522
JAYSH <sanjaysheombar@gmail.com>
2025-05-10 21:36:42 +0200

    changed weight input to decimal keyboard

--- feature/weightmanagement/src/main/java/nl/jaysh/journal/feature/weightmanagement/WeightManagementScreen.kt
+++ feature/weightmanagement/src/main/java/nl/jaysh/journal/feature/weightmanagement/WeightManagementScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
@@ -33,6 +34,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -162,6 +165,10 @@ private fun WeightManagementBottomSheet(
             label = "Weight",
             suffix = { Text("KG") },
             icon = R.drawable.ic_scale,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Decimal,
+                imeAction = ImeAction.Next,
+            ),
             onValueChange = { weight -> onEvent(OnChangeWeight(weight)) },
         )
 
